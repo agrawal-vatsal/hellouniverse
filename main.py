e024e5f460c58c3541613383bb24a2eec9fb6196
dhruv0503 <dhruv2505.dag@gmail.com>
2022-10-04 01:46:33 +0530

    Some random words added

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
     print("Final is here")
     print("Yo Yo 2")
     print("Firse Yo Yo")
-    print("Dunia hugs")
+    print("Dunia is madarchod")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
